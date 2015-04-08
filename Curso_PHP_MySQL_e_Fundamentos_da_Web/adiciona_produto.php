b2128f1de0de95f9518fd283d8ce011f00551c44
Marcio Antunes <cgslivre@cgslivre.com>
2015-04-08 13:14:53 -0300

    Create adiciona_produto.php

---
+++ Curso_PHP_MySQL_e_Fundamentos_da_Web/adiciona_produto.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+	</head>
+<body>
+	<h1>Cadastro de produto</h1>
+	<form action="adiciona-produto.php" method="post">
+
+		<label for="nome">Nome do produto:</label>
+		<input type="text" name="nome" value="" />
+
+		<label for="preco">Preço do produto:</label>
+		<input type="text" name="preco" value="" />	
+
+		<br />
+		<input type="submit" value="Salvar" />
+
+	</form>
+</body>
+</html>
