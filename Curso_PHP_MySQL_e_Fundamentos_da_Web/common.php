ad702318a9b85cef7d7a1f3af18d87ce60684c9c
Marcio Antunes <cgslivre@cgslivre.com>
2015-04-08 18:20:26 -0300

    Update common.php

--- Curso_PHP_MySQL_e_Fundamentos_da_Web/common.php
+++ Curso_PHP_MySQL_e_Fundamentos_da_Web/common.php
@@ -1,7 +1,12 @@
 <?php
+// Secao de definições
+// Servidor
 define('DB_HOST', 'localhost');
+// Usuario
 define('DB_USER', 'root');
+// Senha
 define('DB_PASS', '');
+// Base de dados
 define('DB_BASE', 'alura_php');
 
 // Conexão com o banco de dados
@@ -10,35 +15,17 @@ function getDbConnection(){
 	return $connection;
 }
 
-// Selecao de dados
-function dbSelect($tableName = null, $columns = '*', $conditions = array(), $limit = null){
-
-}
-
-// Inserção de dados
-function dbInsert($tableName = null, $arrayFieldValue = array){
-	
-}
-
-// Atualização de dados
-function dbUpdate(){
-
-}
-
-// Deleção de dados
-function dbDelete(){
-
-}
-
+// Executa e retorna o resultado de uma instrução SQL
 function dbQuery($sql = null){
-	return mysqli_query(getDbConnection(), $sql);
+	$query = mysqli_query(getDbConnection(), $sql);
+	return $query;
 }
+
 // Insere Produto
 function insereProduto($nome, $preco) {
-	$sql = "insert into produtos (nome, preco) values ('{$nome}', ${preco})";
+	// Preparando a SQL
+	$sql = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {preco})";
   $result = dbQuery($sql);
   return $result;
 }
-
-
 ?>
