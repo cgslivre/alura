54fe7f9e685758e8d59dad72a6eef0de9705fb21
cgslivre <cgslivre@cgslivre.com>
2015-04-21 15:54:39 -0300

    Alteração do fechamento do arquivo common.php

--- Curso_PHP_MySQL_e_Fundamentos_da_Web/common.php
+++ Curso_PHP_MySQL_e_Fundamentos_da_Web/common.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('display_errors', 1);
 // Secao de definições
 // Servidor
 define('DB_HOST', 'localhost');
@@ -9,23 +10,22 @@ define('DB_PASS', '');
 // Base de dados
 define('DB_BASE', 'alura_php');
 
+function showDbError($error){
+	exit($error);
+}
 // Conexão com o banco de dados
 function getDbConnection(){
-	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_BASE);
+	$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_BASE) or die(showDbError(mysqli_connect_error()));
 	return $connection;
 }
 
 // Executa e retorna o resultado de uma instrução SQL
 function dbQuery($sql = null){
-	$query = mysqli_query(getDbConnection(), $sql);
+	$connection = getDbConnection();
+	$query = mysqli_query($connection, $sql) or die(showDbError(mysqli_error($connection)));
 	return $query;
 }
 
-// Insere Produto
-function insereProduto($nome, $preco) {
-	// Preparando a SQL
-	$sql = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
-  	$result = dbQuery($sql);
-  	return $result;
+function pr($mixed){
+	echo '<pre>'.print_r($mixed, true).'</pre>';
 }
-?>
